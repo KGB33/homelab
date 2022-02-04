24374203136226a66e48ab25e708f87780f2ea17
kgb33 <keltonbassingthwaite@gmail.com>
2022-02-04 11:05:44 -0800

    added pipx packages

--- package-list.sh
+++ package-list.sh
@@ -11,3 +11,9 @@ pacman -Qqe \
 	| grep -v "$(pacman -Qqm)" \
 	| awk 'BEGIN{print "pacman_packages:"}; {printf"  - %s\n", $1};' \
 	> vars/pacman_packages.yml
+
+# Generate pipx package list
+pipx list \
+	| grep "package" \
+	| awk 'BEGIN{print "pipx_packages:"}; {printf"  - %s\n", $2};' \
+	> vars/pipx_packages.yml
