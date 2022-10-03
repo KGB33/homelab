f62341a6d6dcd7da8647cfb35067b7a799775891
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-10-03 13:13:46 -0700

    now creates file if it doesn't exist

--- scripts/homed-parsing.py
+++ scripts/homed-parsing.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     cmd = subprocess.run(["homectl", "inspect", "-EE"], capture_output=True, check=True)
     data = json.loads(cmd.stdout)
     del data["privileged"]
-    with open("vars/kgb33.identity", "w") as file:
+    with open("vars/kgb33.identity", "w+") as file:
         json.dump(data, file, indent=4)
