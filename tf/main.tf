3ebdde2e9db9adf1719be382abda092995eb00eb
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-01-11 21:48:23 -0800

    feat(k8s): Defined six K8 nodes in terraform.

--- tf/main.tf
+++ tf/main.tf
@@ -1,22 +1,42 @@
-# VM-1
-resource "proxmox_vm_qemu" "cloudinit-test" {
+resource "proxmox_vm_qemu" "k8s-VMs" {
   for_each = {
-    foo = {
+    # Glint
+    gnar = {
       ip      = "10.0.0.112",
       macaddr = "22:d4:92:84:f1:bb",
       id      = 501,
       node    = "glint"
     },
-    bar = {
+    gwen = {
       ip      = "10.0.0.113",
       macaddr = "22:d4:92:84:f1:cc",
       id      = 502,
       node    = "glint"
     },
-    baz = {
+    # Sundance
+    sion = {
       ip      = "10.0.0.114",
       macaddr = "22:d4:92:84:f1:dd",
       id      = 503,
+      node    = "sundance"
+    },
+    shen = {
+      ip      = "10.0.0.115",
+      macaddr = "22:d4:92:84:f1:ee",
+      id      = 504,
+      node    = "sundance"
+    },
+    # Targe
+    teemo = {
+      ip      = "10.0.0.116",
+      macaddr = "22:d4:92:84:f1:ff",
+      id      = 505,
+      node    = "targe"
+    },
+    twitch = {
+      ip      = "10.0.0.117",
+      macaddr = "22:d4:92:84:f1:11",
+      id      = 506,
       node    = "targe"
     }
   }
@@ -49,13 +69,13 @@ resource "proxmox_vm_qemu" "cloudinit-test" {
   }
 
   provisioner "file" {
-    source      = "./provisioners/highstate-${each.key}.yaml"
+    source      = "./provisioners/highstate.yaml"
     destination = "highstate.yaml"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 highstate.yaml -e 'ansible_become_password=${var.become_password}'",
+      "ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 highstate.yaml -e 'ansible_become_password=${var.become_password}' --extra-vars 'host=${each.key}'",
     ]
   }
   provisioner "remote-exec" {
