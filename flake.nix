f9d4f37819c9f1bb87420a9249817fccb632b2d0
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-04-21 14:43:33 -0700

    feat(nix): Added most tools to devShell

--- flake.nix
+++ flake.nix
@@ -2,23 +2,36 @@
   description = "Homelab toolbox";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable-small"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = {
-          default = pkgs.hello;
-        };
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pyPkgs = pkgs.python311Packages;
 
+      in
+      {
         # enable `nix fmt`
         formatter = pkgs.nixpkgs-fmt;
 
         devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.hello pkgs.cowsay ];
+          buildInputs = [
+            pyPkgs.ansible-core
+            pyPkgs.ansible-lint
+            pyPkgs.ansible
+            pkgs.terraform
+            pkgs.talosctl
+            pkgs.kubectl
+            pkgs.kubeseal
+            pkgs.yq
+            pkgs.argocd
+            pkgs.jsonnet
+            pkgs.jsonnet-bundler
+            pkgs.just
+          ];
         };
 
       });
