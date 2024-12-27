b3d5ce6d5678c4e2d36987ac0d9a0584c39d81da
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-12-27 13:11:39 -0800

    Update network config.

--- nixos/hosts/ophiuchus/configuration.nix
+++ nixos/hosts/ophiuchus/configuration.nix
@@ -11,8 +11,11 @@
 
   systemd.network = {
     enable = true;
+    netdevs."09-vlan".vlanConfig = {Id = 9;};
     networks."10-enp0s13f0u1" = {
       matchConfig.Name = "enp0s13f0u1";
+      gateway = ["10.0.9.1"];
+      vlan = ["09-vlan"];
       networkConfig = {
         Address = "10.0.9.104/24";
       };
