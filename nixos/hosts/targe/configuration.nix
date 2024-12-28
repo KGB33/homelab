0cfbcb76ec956321af2803a6c5892d333a877389
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-12-28 13:15:17 -0800

    Add targe host

--- nixos/hosts/targe/configuration.nix
+++ nixos/hosts/targe/configuration.nix
@@ -1,16 +1,39 @@
-{config, ...}: {
-  imports = [../base/configuration.nix];
+{...}: {
+  imports = [
+    ../base/configuration.nix
+    ./disks.nix
+  ];
 
   networking = {
     hostName = "targe";
+    hostId = "5768368a"; # `head -c4 /dev/urandom | od -A none -t x4`
   };
 
   systemd.network = {
     enable = true;
-    networks."10-ens0" = {
-      matchConfig.Name = "ens0";
-      networkConfig = {
-        Address = "10.0.9.102/24";
+    netdevs = {
+      "10-vlan9" = {
+        netdevConfig = {
+          Name = "vlan9";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 9;
+      };
+    };
+    networks = {
+      "10-enp" = {
+        matchConfig.Name = "enp7s0f";
+        vlan = ["vlan9"];
+        networkConfig.LinkLocalAddressing = "no";
+        linkConfig.RequiredForOnline = "carrier";
+      };
+
+      "10-vlan9" = {
+        matchConfig.Name = "vlan9";
+        gateway = ["10.0.9.1"];
+        networkConfig = {
+          Address = "10.0.9.102/24";
+        };
       };
     };
   };
