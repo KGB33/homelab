c4b8a218c737b96b4de6b0db091bda322a061e64
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-02-02 18:50:07 -0800

    Fix promtail config syntax.

--- nixos/hosts/ophiuchus/configuration.nix
+++ nixos/hosts/ophiuchus/configuration.nix
@@ -259,21 +259,29 @@
         grpc_listen_port = 0;
       };
       positions.filename = "/tmp/promtail.positions.yaml";
-      clients.url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
-      srape_configs = {
-        job_name = "journal";
-        journal = {
-          max_age = "12h";
-          labels = {
-            job = "systemd-journal";
-            host = config.networking.hostName;
+      clients = [
+        {
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        }
+      ];
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = config.networking.hostName;
+            };
           };
-        };
-        relabel_configs = {
-          source_labels = ["__journal__systemd_unit"];
-          target_label = "unit";
-        };
-      };
+          relabel_configs = [
+            {
+              source_labels = ["__journal__systemd_unit"];
+              target_label = "unit";
+            }
+          ];
+        }
+      ];
     };
   };
 }
