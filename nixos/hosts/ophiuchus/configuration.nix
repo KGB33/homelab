34e7ffd3597533c3b1409b622f96e85245d45888
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-02-02 16:25:19 -0800

    Updated loki config.

--- nixos/hosts/ophiuchus/configuration.nix
+++ nixos/hosts/ophiuchus/configuration.nix
@@ -213,34 +213,27 @@
 
   services.loki = {
     enable = true;
-    configuration = {
+    configuration = let
+      lokiDir = "/tmp/loki";
+    in {
       server.http_listen_port = 3030;
       auth_enabled = false;
 
-      ingester = {
-        lifecycler = {
-          address = "127.0.0.1";
-          ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-            replication_factor = 1;
-          };
+      common = {
+        ring = {
+          instance_addr = "127.0.0.1";
+          kvstore.store = "inmemory";
+          replication_factor = 1;
+          path_prefix = lokiDir;
         };
-        chunk_idle_period = "1h";
-        max_chunk_age = "1h";
-        chunk_target_size = 999999;
-        chunk_retain_period = "30s";
-        max_transfer_retries = 0;
       };
-
       schema_config = {
         configs = [
           {
             from = "2025-01-01";
-            store = "boltdb-shipper";
+            store = "tsdb";
             object_store = "filesystem";
-            schema = "v11";
+            schema = "v13";
             index = {
               prefix = "index_";
               period = "24h";
@@ -248,42 +241,9 @@
           }
         ];
       };
-
-      storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-          cache_location = "/var/lib/loki/boltdb-shipper-cache";
-          cache_ttl = "24h";
-          shared_store = "filesystem";
-        };
-
-        filesystem = {
-          directory = "/var/lib/loki/chunks";
-        };
-      };
-
-      limits_config = {
-        reject_old_samples = true;
-        reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = {
-        max_look_back_period = "0s";
-      };
-
-      table_manager = {
-        retention_deletes_enabled = false;
-        retention_period = "0s";
-      };
-
-      compactor = {
-        working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
-        compactor_ring = {
-          kvstore = {
-            store = "inmemory";
-          };
-        };
+      storage_config.filesystem.directory = "${lokiDir}/chunks";
+      compactior = {
+        working_directory = "${lokiDir}/compactor";
       };
     };
   };
@@ -296,7 +256,7 @@
         grpc_listen_port = 0;
       };
       positions.filename = "/tmp/promtail.positions.yaml";
-      clients.url = "http://127.0.0.1:${toString config.services.loki.configuraiton.server.http_listen_port}/loki/api/v1/push";
+      clients.url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
       srape_configs = {
         job_name = "journal";
         journal = {
