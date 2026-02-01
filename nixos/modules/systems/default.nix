c3bad4c58cbabd377f650a265c5b5b750af3e33c
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-02-01 08:20:48 -0800

    feat(nixos): Podman setup

--- nixos/modules/systems/default.nix
+++ nixos/modules/systems/default.nix
@@ -2,6 +2,8 @@
   flake.modules.nixos.system-default = {...}: {
     imports = with inputs.self.modules.nixos; [
       system-minimal
+      podman
+      comin
       user-kgb33
       ssh
     ];
