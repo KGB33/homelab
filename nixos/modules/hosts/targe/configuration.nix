7de06f661f3c904b370c0340c44c51bc396989f4
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-03-22 17:12:17 -0700

    migrate targe

---
+++ nixos/modules/hosts/targe/configuration.nix
@@ -0,0 +1,36 @@
+{inputs, ...}: {
+  flake.modules.nixos.targe = {...}: {
+    imports = with inputs.self.modules.nixos; [
+      system-default
+      wireguard
+      hickory-dns
+    ];
+
+    networking = {
+      hostName = "targe";
+      hostId = "5768368a";
+      useNetworkd = true;
+    };
+
+    systemd.network = {
+      enable = true;
+      netdevs."10-vlan9" = {
+        netdevConfig = {Name = "vlan9"; Kind = "vlan";};
+        vlanConfig.Id = 9;
+      };
+      networks = {
+        "10-enp" = {
+          matchConfig.Name = "enp7s0f1";
+          vlan = ["vlan9"];
+          networkConfig.LinkLocalAddressing = "no";
+          linkConfig.RequiredForOnline = "carrier";
+        };
+        "10-vlan9" = {
+          matchConfig.Name = "vlan9";
+          gateway = ["10.0.9.1"];
+          addresses = [{Address = "10.0.9.102/24";}];
+        };
+      };
+    };
+  };
+}
