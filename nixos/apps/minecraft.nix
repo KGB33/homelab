a7ba5348d85b2ea23011302676fda5cee84c2fb6
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-03-24 10:30:16 -0700

    Add CC-Tweaked to Monifactory

--- nixos/apps/minecraft.nix
+++ nixos/apps/minecraft.nix
@@ -2,81 +2,94 @@
   config,
   lib,
   ...
-}: let
-  mkMinecraft = {
-    slug,
-    port,
-    path,
-  }: {
-    image = "ghcr.io/itzg/minecraft-server";
-    pull = "newer";
-    environment = {
-      MODPACK_PLATFORM = "AUTO_CURSEFORGE";
-      CF_SLUG = slug;
-      EULA = "TRUE";
-      MAX_MEMORY = "28G";
-    };
-    environmentFiles = [
-      config.sops.secrets.curseForgeToken.path
-    ];
-    ports = [
-      "${toString port}:${toString port}"
-    ];
-    volumes = [
-      "/home/kgb33/Minecraft/${path}/:/data"
-    ];
-  };
-  packs = [
-    # {
-    #   slug = "all-the-mods-10";
-    #   port = 25565;
-    #   path = "ATM10";
-    # }
-    # {
-    #   slug = "all-the-mods-10-sky";
-    #   port = 25566;
-    #   path = "ATM10tts";
-    # }
-    # {
-    #   slug = "ftb-stoneblock-4";
-    #   port = 25568;
-    #   path = "Stoneblock4";
-    # }
-    {
-      slug = "monifactory";
-      port = 25569;
-      path = "monifactory";
-    }
-  ];
-in {
-  networking.firewall.allowedTCPPorts = (map (p: p.port) packs) ++ [25567 24454];
+}: {
+  networking.firewall.allowedTCPPorts = [25565 25566 25568 25569 25567 24454];
 
-  virtualisation.oci-containers.containers =
-    builtins.listToAttrs (map (p: {
-        name = p.slug;
-        value = mkMinecraft p;
-      })
-      packs)
-    // {
-      "silasOrigins" = {
-        image = "ghcr.io/itzg/minecraft-server";
-        pull = "newer";
-        environment = {
-          EULA = "TRUE";
-          VERSION = "1.20.1";
-          MAX_MEMORY = "20G";
-          TYPE = "FORGE";
-          PACKWIZ_URL = "https://raw.githubusercontent.com/FrostyTacos/SilasOriginsPack/refs/heads/main/pack.toml";
-        };
-        ports = [
-          "25567:25565"
-          "24454:24454/udp"
-        ];
-        volumes = [
-          "/home/kgb33/Minecraft/SilasOrigins/:/data"
-        ];
+  virtualisation.oci-containers.containers = {
+    # "all-the-mods-10" = {
+    #   image = "ghcr.io/itzg/minecraft-server";
+    #   pull = "newer";
+    #   environment = {
+    #     MODPACK_PLATFORM = "AUTO_CURSEFORGE";
+    #     CF_SLUG = "all-the-mods-10";
+    #     EULA = "TRUE";
+    #     MAX_MEMORY = "28G";
+    #   };
+    #   environmentFiles = [
+    #     config.sops.secrets.curseForgeToken.path
+    #   ];
+    #   ports = ["25565:25565"];
+    #   volumes = ["/home/kgb33/Minecraft/ATM10/:/data"];
+    # };
+
+    # "all-the-mods-10-sky" = {
+    #   image = "ghcr.io/itzg/minecraft-server";
+    #   pull = "newer";
+    #   environment = {
+    #     MODPACK_PLATFORM = "AUTO_CURSEFORGE";
+    #     CF_SLUG = "all-the-mods-10-sky";
+    #     EULA = "TRUE";
+    #     MAX_MEMORY = "28G";
+    #   };
+    #   environmentFiles = [
+    #     config.sops.secrets.curseForgeToken.path
+    #   ];
+    #   ports = ["25566:25566"];
+    #   volumes = ["/home/kgb33/Minecraft/ATM10tts/:/data"];
+    # };
+
+    # "ftb-stoneblock-4" = {
+    #   image = "ghcr.io/itzg/minecraft-server";
+    #   pull = "newer";
+    #   environment = {
+    #     MODPACK_PLATFORM = "AUTO_CURSEFORGE";
+    #     CF_SLUG = "ftb-stoneblock-4";
+    #     EULA = "TRUE";
+    #     MAX_MEMORY = "28G";
+    #   };
+    #   environmentFiles = [
+    #     config.sops.secrets.curseForgeToken.path
+    #   ];
+    #   ports = ["25568:25568"];
+    #   volumes = ["/home/kgb33/Minecraft/Stoneblock4/:/data"];
+    # };
+
+    "monifactory" = {
+      image = "ghcr.io/itzg/minecraft-server";
+      pull = "newer";
+      environment = {
+        MODPACK_PLATFORM = "AUTO_CURSEFORGE";
+        CF_SLUG = "monifactory";
+        EULA = "TRUE";
+        MAX_MEMORY = "28G";
+        MODRINTH_PROJECTS = "cc-tweaked";
       };
+      environmentFiles = [
+        config.sops.secrets.curseForgeToken.path
+      ];
+      ports = ["25569:25569"];
+      volumes = ["/home/kgb33/Minecraft/monifactory/:/data"];
     };
+
+    "silasOrigins" = {
+      image = "ghcr.io/itzg/minecraft-server";
+      pull = "newer";
+      environment = {
+        EULA = "TRUE";
+        VERSION = "1.20.1";
+        MAX_MEMORY = "20G";
+        TYPE = "FORGE";
+        PACKWIZ_URL = "https://raw.githubusercontent.com/FrostyTacos/SilasOriginsPack/refs/heads/main/pack.toml";
+      };
+      ports = [
+        "25567:25565"
+        "24454:24454/udp"
+      ];
+      volumes = [
+        "/home/kgb33/Minecraft/SilasOrigins/:/data"
+      ];
+    };
+  };
 
   sops = {
     secrets = {
